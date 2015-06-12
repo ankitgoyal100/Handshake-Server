b01248dfa86cc4ead5636985c5ef8aad18c01ccc
Sam Ober <ober.sam@gmail.com>
2015-06-12 15:09:06 -0400

    add contact_updated field to search results

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -17,6 +17,7 @@ module SearchHelper
       json.cards friendship.cards do |card|
         json_card(json, card)
       end
+      json.contact_updated friendship.updated_at
       json.request_sent false
       json.request_received false
     else
