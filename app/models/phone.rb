994dfbfa20afa4cfbf0de68673be1bd33f42543b
Sam Ober <ober.sam@gmail.com>
2015-06-15 16:56:14 -0400

    normalize phone numbers

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -2,7 +2,9 @@ class Phone < ActiveRecord::Base
   
   belongs_to :card
   
+  phony_normalize :number
+  
   validates_plausible_phone :number, presence: true
   validates :country_code, presence: true
-  
+
 end
