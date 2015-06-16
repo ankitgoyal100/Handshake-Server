04401974692ab202c3f6abb34b79d68c0010737b
Sam Ober <ober.sam@gmail.com>
2015-06-16 16:42:56 -0400

    fix suggestions bug

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -10,18 +10,18 @@ class SuggestionsController < ApplicationController
         Phone.where(number: entry.phone).each do |phone|
           if phone
             user = phone.card.user
-            @suggestions << user if user != current_user and not Friendship.find_by(user: current_user, contact: user, is_deleted: false) and not Friendship.find_by(user: user, contact: current_user, is_deleted: false)
+            @suggestions << user if user != current_user and not Friendship.find_by(user: current_user, contact: user) and not Friendship.find_by(user: user, contact: current_user)
           end
         end
       elsif entry.email
         Email.where(address: entry.email).each do |email|
           if email
             user = email.card.user
-            @suggestions << user if user != current_user and not Friendship.find_by(user: current_user, contact: user, is_deleted: false) and not Friendship.find_by(user: user, contact: current_user, is_deleted: false)
+            @suggestions << user if user != current_user and not Friendship.find_by(user: current_user, contact: user) and not Friendship.find_by(user: user, contact: current_user)
           end
         end
         User.where(email: entry.email) do |user|
-          @suggestions << user if user != current_user and not Friendship.find_by(user: current_user, contact: user, is_deleted: false) and not Friendship.find_by(user: user, contact: current_user, is_deleted: false)
+          @suggestions << user if user != current_user and not Friendship.find_by(user: current_user, contact: user) and not Friendship.find_by(user: user, contact: current_user)
         end
       end
     end
