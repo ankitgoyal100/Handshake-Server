c6bf8795b5b362f43957ecec4d7bda8b9566f9ca
Sam Ober <ober.sam@gmail.com>
2015-08-31 23:54:19 -0400

    updated home screen and fixed image resizing

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -44,10 +44,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   process :resize_to_fit => [50, 50]
   # end
   
-  # process :resize_to_fill => [200, 200]
+  process :resize_to_fit => [800, 800]
   
   version :thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fill => [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
